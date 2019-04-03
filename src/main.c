c1b27652939fd9f7a6ab44683c8da2c50b93498d
Richard <richard.anaya@gmail.com>
2019-04-03 00:38:33 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 // you would use this variable to write your own malloc/free
 extern unsigned char __heap_base;
 
-
 export void *get_heap_base() { return &__heap_base; }
 
 export int add(int x, int y) { return x + y; }
