765e8d7e3a24f86a57b3d66789ed7fb27903b81a
Richard <Richard>
2019-04-03 07:09:40 +0000

    adding lint

--- src/main.c
+++ src/main.c
@@ -1,14 +1,8 @@
 #define export __attribute__((visibility("default")))
 extern unsigned char __heap_base;
 
-export void * get_heap_base() {
-  return &__heap_base;
-}
+export void *get_heap_base() { return &__heap_base; }
 
-export int add(int x, int y) {
-  return x+y;
-}
+export int add(int x, int y) { return x + y; }
 
-export int sub(int x, int y) {
-  return x - y;
-}
+export int sub(int x, int y) { return x - y; }
