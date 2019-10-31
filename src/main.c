b2c401080e6e2d3ebceb9f508fdae7ffffe82de9
Richard Anaya <richard.anaya@gmail.com>
2019-10-31 14:42:05 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,4 @@
 // a simple way to export your functions
 #define export __attribute__((visibility("default")))
 
-// you would use this variable to write your own malloc/free
-extern unsigned char __heap_base;
-
-export void *get_heap_base() { return &__heap_base; }
-
 export int add(int x, int y) { return x + y; }
